ed021965bec079b679b24bf799720e3561f50187
Ben Robie <brobie@degdigital.com>
2016-07-20 15:19:50 -0500

    Adding the ability to get the products of the subcategories.

--- app/code/local/DEG/SubcategoryView/Block/Subcategory.php
+++ app/code/local/DEG/SubcategoryView/Block/Subcategory.php
@@ -37,4 +37,40 @@ class DEG_SubcategoryView_Block_Subcategory extends Mage_Core_Block_Template
     {
         return $this->_getHelper()->getVisibleProductCount($category);
     }
+
+    /**
+     * Gets the current Layer
+     *
+     * @return Mage_Catalog_Model_Layer
+     */
+    protected function getLayer()
+    {
+        if (Mage::helper('enterprise_search')->getIsEngineAvailableForNavigation()) {
+            return Mage::getSingleton('enterprise_search/catalog_layer');
+        } else {
+            return Mage::getSingleton('catalog/layer');
+        }
+    }
+
+    /**
+     * Returns all of the visible products and their attributes for a given category.
+     * @param $category
+     * @return Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Collection
+     */
+    public function getVisibleProductCollection($category){
+        $layer = $this->getLayer();
+        $layer->setCurrentCategory($category);
+        return $layer->getProductCollection();
+    }
+
+    /**
+     * Gets the product URL without the hierarchical category information.
+     *
+     * @param $product
+     * @return mixed
+     */
+    public function getProductUrlWithoutCategory($product){
+        return Mage::getResourceSingleton('catalog/product')->getAttributeRawValue($product->getId(), 'url_key', Mage::app()->getStore());
+    }
+
 }
